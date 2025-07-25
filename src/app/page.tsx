f043985512e1643fc9c42190df9cf875920008a8
DiegoGarciaCo <diegogarcia51916@gmail.com>
2025-07-25 18:53:49 -0500

    debugging

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,13 @@
 
 import PostList from "@/components/PostList";
 import SidebarNav from "@/components/Sidebar";
-import { ListAllPostsRow } from "@/lib/definitions";
 import { fetchPosts } from "@/lib/data";
 
 export const dynamic = "force-dynamic"; // Force dynamic rendering
 
-const getPosts = async (): Promise<ListAllPostsRow[]> => {
-    return fetchPosts();
-}
 
 export default async function AdminHome() {
-    const posts = await getPosts();
+    const posts = await fetchPosts();
     console.log("Posts fetched:", posts);
   return (
     <main className="flex min-h-screen bg-brand-neutral text-brand-primary">
