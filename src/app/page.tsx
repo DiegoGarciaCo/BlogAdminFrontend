3efe692c11b89cb977ee4b50310b0a45e72ffb2f
DiegoGarciaCo <diegogarcia51916@gmail.com>
2025-07-25 18:06:16 -0500

    fetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,24 @@
 
 import PostList from "@/components/PostList";
 import SidebarNav from "@/components/Sidebar";
+import { ListAllPostsRow } from "@/lib/definitions";
+import { fetchPosts } from "@/lib/data";
 
 export const dynamic = "force-dynamic"; // Force dynamic rendering
 
-export default function AdminHome() {
+async function getPosts(): Promise<ListAllPostsRow[]> {
+  return fetchPosts();
+}
+
+export default async function AdminHome() {
+    const posts = await getPosts();
+    console.log("Posts fetched:", posts);
   return (
     <main className="flex min-h-screen bg-brand-neutral text-brand-primary">
       <SidebarNav />
       <section className="flex-1 p-6">
         <h1 className="text-3xl font-bold mb-6">Posts</h1>
-        <PostList />
+        <PostList posts={posts}/>
       </section>
     </main>
   );
--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,14 +1,8 @@
 import PostFilter from "./PostFilter";
 import { ListAllPostsRow } from "@/lib/definitions";
-import { fetchPosts } from "@/lib/data";
 
-async function getPosts(): Promise<ListAllPostsRow[]> {
-  return fetchPosts();
-}
 
-export default async function PostList() {
-  const posts = await getPosts();
-  console.log("Fetched posts:", posts);
+export default async function PostList( { posts }: { posts: ListAllPostsRow[] }) {
   return (
     <div className="mt-6">
       <PostFilter posts={posts} />
